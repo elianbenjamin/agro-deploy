614040c3d4271e1d5e9a002559d1c19d695ce21d
elianbenjamin <elianbenjamin30@gmail.com>
2024-07-08 23:35:31 -0300

    ultimos cambios

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -22,7 +22,7 @@ const About = () => {
 
   const handleImageClick = (image: string) => {
     setImage(image);
-    setPageId('about');
+    setPageId("about");
     navigate("/fullsize-image");
   };
 
@@ -34,15 +34,14 @@ const About = () => {
         <h1>Nuestra Historia</h1>
 
         <p className={styles.text_on}>
-          En 2006-2007, nuestra empresa comenzó con 500 hectáreas de terreno, 5
-          camiones incluyendo mercedes 1518 y ford volcadores, y 1 picadora. A
-          través de un servicio excepcional que generó un boca a boca positivo,
-          experimentamos un crecimiento gradual. En ese entonces, éramos un
-          equipo de 7 empleados. Nuestra trayectoria empresarial se centra en el
-          picado de forraje, destacándonos por la calidad de nuestro servicio y
-          la satisfacción del cliente. A medida que la demanda creció,
-          expandimos nuestras operaciones y la capacidad de picado para
-          satisfacer las necesidades del mercado.
+          En 2006-2007 con mucho esfuerzo y tiempo de aprendizaje, A través de
+          un servicio excepcional que generó un boca a boca positivo,
+          experimentamos un crecimiento gradual. En ese entonces, Nuestra
+          trayectoria empresarial se centra en el picado de forraje,
+          destacándonos por la calidad de nuestro servicios y la satisfacción del
+          cliente. A medida que la demanda creció, expandimos nuestras
+          operaciones y la capacidad de picado para satisfacer las necesidades
+          del mercado.
         </p>
 
         <NavLink
